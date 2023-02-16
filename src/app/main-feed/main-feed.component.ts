48abf2d4d37e6550fdc2484d7a2c3d232a2a6ad0
zebn <zebn@bk.ru>
2023-02-16 19:55:46 +0100

    session 2

--- src/app/main-feed/main-feed.component.ts
+++ src/app/main-feed/main-feed.component.ts
@@ -1,4 +1,5 @@
-import { Component } from '@angular/core';
+import { Component, OnDestroy } from '@angular/core';
+import { Subscription } from 'rxjs';
 import { Tweet } from '../share/model/tweet';
 import { TweetService } from '../tweet.service';
 
@@ -7,16 +8,24 @@ import { TweetService } from '../tweet.service';
   templateUrl: './main-feed.component.html',
   styleUrls: ['./main-feed.component.scss']
 })
-export class MainFeedComponent {
+export class MainFeedComponent implements OnDestroy {
 
   title = 'twitter3-web3';
 
   public tweets: Tweet[] = [];
+  public subscriptions: Subscription[] = new Array<Subscription>();
+  public subscription: Subscription = new Subscription();
 
   public constructor(public tweetService: TweetService) {
     this.tweets = this.tweetService.getTweets();
+    this.subscription = this.tweetService.newTweets$.subscribe(() => {
+      this.tweets = this.tweetService.getTweets();
+    })
 
+  }
 
+  public ngOnDestroy(): void {
+    this.subscription.unsubscribe();
   }
 
 }
